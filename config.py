35dc57e0c6630dce34525288776a67263d2c2b8d
vlflex <lecsinvlad@mail.ru>
2024-02-16 10:04:58 +0300

    config: add secret strs

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ from pydantic import SecretStr
 
 class Settings(BaseSettings):
     bot_token: SecretStr
+    email: SecretStr
+    password: SecretStr
     model_config = SettingsConfigDict(env_file = '.env', 
                                     env_file_encoding = 'utf-8')
     
