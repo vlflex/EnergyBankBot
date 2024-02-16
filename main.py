7d894d66f08d60f8767c03a458415a3543dbfec1
vlflex <lecsinvlad@mail.ru>
2024-02-16 10:05:31 +0300

    main: add sentry logging

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ from pydantic import SecretStr
 import sentry_sdk
 from string import Template
 from database import DataBase
+import sentry_sdk
 
-# sentry
-
-# secret str
-bot: Bot = Bot('6546159525:AAE0G11LWoL6ZzvqvzlzAkoHqUBtq6DoZ6c')
+sentry_sdk.init(
+    dsn="https://77424977a50d8cd32312624d13bc12f8@o4506209236484096.ingest.sentry.io/4506755171352576",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 dp: Dispatcher = Dispatcher()
 
 @dp.message(Command('start'))
