ec5cc5d061c63d8a4298b41ffb81961db8725886
vlflex <lecsinvlad@mail.ru>
2024-04-03 23:38:36 +0300

    logger: expand add new secret values

--- config.py
+++ config.py
@@ -1,16 +1,25 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
 from cryptography.fernet import Fernet
+from logging import DEBUG, INFO
 
 class Settings(BaseSettings):
     bot_token: SecretStr
     email: SecretStr
     password: SecretStr
     code_key: SecretStr
-    model_config = SettingsConfigDict(env_file = '.env', 
+    db_name: SecretStr
+    db_user: SecretStr
+    db_pass: SecretStr
+    db_host: SecretStr
+    db_port: SecretStr
+    model_config = SettingsConfigDict(env_file = 'data/.env', 
                                     env_file_encoding = 'utf-8')
     
 config = Settings() # type: ignore
+
+# общая настройка уровня логгирования 
+LOG_LEVEL = DEBUG
 
 # шифрование данных
 def get_ciphered(data: str):
