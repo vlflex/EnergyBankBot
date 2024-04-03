e41846f930dacc7fee563307a9da3554110203d9
vlflex <lecsinvlad@mail.ru>
2024-04-03 23:37:02 +0300

    delete user, replace to nametuple

--- user.py
+++
@@ -1,8 +0,0 @@
-from collections import namedtuple
-
-TupleUser = namedtuple('User', ['id', 'pin', 'email', 'authorized'])
-
-class User(TupleUser):
-    # получение всех атрибутов
-    def get(self):
-        return (self.id, self.pin, self.email, self.authorized)
