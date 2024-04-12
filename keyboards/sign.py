65a65fbaa60b76cb1f528315def4abd18d766a08
vlflex <lecsinvlad@mail.ru>
2024-04-12 21:40:23 +0300

    sign: create kb builder to DRY

--- keyboards/sign.py
+++ keyboards/sign.py
@@ -1,20 +1,19 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from typing import Sequence
 
-# клавиатура с "да" и "нет"
-def get_yes_no_keyboard() -> ReplyKeyboardMarkup:
+# конструктор клавиатуры
+def __reply_kb_builder(buttons: Sequence[str], button_in_row: int = 2) -> ReplyKeyboardMarkup:
     builder = ReplyKeyboardBuilder()
-    builder.button(text = 'Да')
-    builder.button(text = 'Нет')
+    for btn in buttons:
+        builder.button(text = btn)
+    builder.adjust(button_in_row)
     markup = builder.as_markup()
     markup.resize_keyboard = True
     return markup
 
-# клавиату с авторизацией и регистрацией
-def get_register_auth_keyboard() -> ReplyKeyboardMarkup:
-    builder = ReplyKeyboardBuilder()
-    builder.button(text = 'Регистрация')
-    builder.button(text = 'Авторизация')
-    markup = builder.as_markup()
-    markup.resize_keyboard = True
-    return markup
+def register_auth_kb():
+    return __reply_kb_builder(['Регистрация', 'Авторизация'])
+
+def register_kb():
+    return __reply_kb_builder(['Регистрация'])
