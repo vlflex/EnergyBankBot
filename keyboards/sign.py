44e1a3c176cc25373934545586f5583a86ffba9a
vlflex <lecsinvlad@mail.ru>
2024-04-13 19:44:03 +0300

    sign: support of one_time_keyboard

--- keyboards/sign.py
+++ keyboards/sign.py
@@ -3,13 +3,14 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from typing import Sequence
 
 # конструктор клавиатуры
-def __reply_kb_builder(buttons: Sequence[str], button_in_row: int = 2) -> ReplyKeyboardMarkup:
+def __reply_kb_builder(buttons: Sequence[str], button_in_row: int = 2, one_time: bool = True) -> ReplyKeyboardMarkup:
     builder = ReplyKeyboardBuilder()
     for btn in buttons:
         builder.button(text = btn)
     builder.adjust(button_in_row)
     markup = builder.as_markup()
     markup.resize_keyboard = True
+    markup.one_time_keyboard = one_time
     return markup
 
 def register_auth_kb():
